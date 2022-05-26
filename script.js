50f2fbf567f9f8349051e9ae4abef689306173f7
Abhishek <abhishekchaudhary234@gmail.com>
2022-05-26 10:53:00 +0530

    Fixed some bugs and improved functionality

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
 let inputDiv = document.querySelector(".input");
+let outputDiv = document.querySelector(".output");
 let numbers = document.getElementsByClassName("number");
 
-Array.from(numbers).forEach(element => {
-    element.addEventListener('click', (e) => {
-        let num = e.target.innerText;
-        if (inputDiv.innerText != 0) {
-            inputDiv.innerText += num;
-        }
-        else inputDiv.innerText = num;            
-     })
-})
+Array.from(numbers).forEach((element) => {
+  element.addEventListener("click", (e) => {
+    let num = e.target.innerText;
+    if (inputDiv.innerText != 0) {
+      inputDiv.innerText += num;
+    } else inputDiv.innerText = num;
+  });
+});
 
 let reset = document.querySelector("#reset");
-reset.addEventListener('click', () => {
-    inputDiv.innerText = '0';
-    store = 0;
-})
-
+reset.addEventListener("click", () => {
+  inputDiv.innerText = "0";
+  store = 0;
+  outputDiv.innerText = "";
+});
 
 let store = 0;
-let operator = '';
+let operator = "";
 
-const operation = (sign,clickedElement) => {
-    if (inputDiv.innerText != '') {
-        clickedElement.style.backgroundColor = "#D4D4D2";
-        if (store == 0) {
-            store += parseFloat(inputDiv.innerText);
-            inputDiv.innerText = "";
-            console.log(store);
-        }
-        else {
-            store = parseFloat(inputDiv.innerText);
-            inputDiv.innerText = "";
-            console.log(store);
-        }
-        operator = sign;
+const operation = (sign) => {
+  if (inputDiv.innerText != "0" || inputDiv.innerText == "") {
+    outputDiv.innerText = sign;
+    if (store == 0) {
+      store += parseFloat(inputDiv.innerText);
+    } else {
+      if (store != parseFloat(inputDiv.innerText) && inputDiv.innerText != "") {
+        store = parseFloat(inputDiv.innerText);
+      }
     }
-}
+    inputDiv.innerText = "";
+    operator = sign;
+    console.log(operator);
+  }
+};
 
 let add = document.querySelector(".add");
 let subtract = document.querySelector(".subtract");
@@ -44,52 +43,41 @@ let multiply = document.querySelector(".multiply");
 let divide = document.querySelector(".divide");
 let percentage = document.querySelector(".percentage");
 
+add.addEventListener("click", operation.bind(this, "+"));
 
-add.addEventListener('click', operation.bind(this, "+", add));
+subtract.addEventListener("click", operation.bind(this, "-"));
 
-subtract.addEventListener('click', operation.bind(this, "-", subtract));
+multiply.addEventListener("click", operation.bind(this, "*"));
 
-multiply.addEventListener('click', operation.bind(this, "*", multiply));
+divide.addEventListener("click", operation.bind(this, "/"));
 
-divide.addEventListener('click', operation.bind(this, "/", divide));
-
-percentage.addEventListener('click', function () {
-    store = store / 100;
-    inputDiv.innerText = store;
+percentage.addEventListener("click", function () {
+  let percent = inputDiv.innerText;
+  store = percent / 100;
+  inputDiv.innerText = store;
+  outputDiv.innerText = "";
 });
 
-
-// needs further enhancements
 let equals = document.querySelector("#equal");
 
-equals.addEventListener('click', function () {
-    let lastEntry = parseFloat(inputDiv.innerText);
-    if (inputDiv.innerText == "") {
-        return;
-    }
-    else if (lastEntry == store) {
-        store = lastEntry;
-        // this needs a function here
-    }
-    else if (operator == "+") {
-        add.style.backgroundColor = "#FF9500";
-        store += lastEntry;
-        inputDiv.innerText = store;
-    }
-    else if (operator == "-") {
-        subtract.style.backgroundColor = "#FF9500";
-        store = store - lastEntry;
-        inputDiv.innerText = store;
-    }
-    else if (operator == "*") {
-        multiply.style.backgroundColor = "#FF9500";
-        store = store * lastEntry;
-        inputDiv.innerText = store;
-    }
-    else if (operator == "/") {
-        divide.style.backgroundColor = "#FF9500";
-        store = store / lastEntry;
-        inputDiv.innerText = store;
-    }
-})
-
+equals.addEventListener("click", function () {
+  let lastEntry = parseFloat(inputDiv.innerText);
+  if (inputDiv.innerText == "") {
+    return;
+  } else if (operator == "+") {
+    add.style.backgroundColor = "#FF9500";
+    store += lastEntry;
+  } else if (operator == "-") {
+    subtract.style.backgroundColor = "#FF9500";
+    store = store - lastEntry;
+  } else if (operator == "*") {
+    multiply.style.backgroundColor = "#FF9500";
+    store = store * lastEntry;
+  } else if (operator == "/") {
+    divide.style.backgroundColor = "#FF9500";
+    store = store / lastEntry;
+  }
+  inputDiv.innerText = store;
+  operator = "";
+  outputDiv.innerText = "";
+});
